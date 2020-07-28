855d898bfc2631dcf38d039224549aa8180571af
Joselyn Chavez <joselynchavez@Joselyns-MacBook-Pro.local>
2020-07-27 22:22:23 -0500

    datasets updated

--- data-raw/kos_tf.R
+++
@@ -1,9 +0,0 @@
-## code to prepare `kos_tf` dataset
-
-kos_tf <- list.files("transcription_factor/")
-
-# save data into the package
-usethis::use_data(kos_tf, overwrite = TRUE)
-
-# usage
-usethis::use_data("kos_tf")
--- data-raw/rfam_riboswitch.R
+++
@@ -1,9 +0,0 @@
-## code to prepare `kos_riboswitch` dataset goes here
-
-rfam_riboswitch <- colnames(data_riboswitch[2:33])
-
-# save data into the package
-usethis::use_data(rfam_riboswitch, overwrite = TRUE)
-
-# usage
-usethis::use_data("rfam_riboswitch")
